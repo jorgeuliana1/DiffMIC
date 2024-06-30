e40b1bc24bcbfec427de270ecd410fbb23ac64be
Jorge Uliana <ulianamjjorge@gmail.com>
2024-06-30 17:17:13 -0300

    Updated OralEpitheliumDB

--- dataloader/loading.py
+++ dataloader/loading.py
@@ -465,9 +465,9 @@ class HIBASixClinicalDataset(Dataset):
             
         return img, int(self.df.loc[index][self.y])    
 
-class LIPAIDataset(Dataset):
-    def __init__(self, root: str, train: bool = True):
-        self.trainsize = (224,224)
+class LIPAIDataset(MyDataset):
+    def __init__(self, root: str, csv_train: str, csv_test: str, train: bool = True, val: bool = False, fold_n: int = -1):
+        super(PadUfes20, self).__init__(root, csv_train, csv_test, train, val, fold_n)
         if train:
             self.transform_center = transforms.Compose([
                 trans.CropCenterSquare(),
@@ -481,30 +481,19 @@ class LIPAIDataset(Dataset):
             self.transform_center = transforms.Compose([
                 trans.CropCenterSquare(),
                 transforms.Resize(self.trainsize),
-                #trans.CenterCrop(self.trainsize),
                 transforms.ToTensor(),
                 transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
             ])
             
-        self.train = train
-        self.root = root
-        
-        # Defining .csv file to be used
-        csv_name = "data.csv"
-            
-        # Opening dataframe:
-        self.df = pd.read_csv(os.path.join(self.root, csv_name), header = 0)
-        self.df["path"] = self.df["diagnosis"] + "/" + self.df["id"] + ".tif"
         self.x = "path"
         self.y = "discretized_diagnosis"
         
         if self.train:
-            self.df = self.df[self.df["train"]].reset_index()
+            self.df = self.df[self.df["fold"] == 0].reset_index()
+        elif self.val:
+            self.df = self.df[self.df["fold"] == 1].reset_index()
         else:
-            self.df = self.df[~self.df["train"]].reset_index()
-
-    def __len__(self):
-        return len(self.df)
+            raise Exception("`train` and `val` can't be both False")
     
     def __getitem__(self, index) -> Tuple[torch.Tensor, int]:
         if torch.is_tensor(index):
@@ -512,7 +501,5 @@ class LIPAIDataset(Dataset):
                 
         img_path = os.path.join(self.root, "images", self.df.loc[index][self.x])
         img = Image.open(img_path).convert('RGB')
-        
         img = self.transform_center(img)
-            
         return img, int(self.df.loc[index][self.y])
