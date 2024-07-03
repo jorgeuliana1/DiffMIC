de3ba7824dec7d83eeee2f80871b07f7f735b6c0
Jorge Uliana <ulianamjjorge@gmail.com>
2024-07-02 21:27:59 -0300

    Fixed key

--- dataloader/loading.py
+++ dataloader/loading.py
@@ -485,7 +485,7 @@ class LIPAIDataset(MyDataset):
                 transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
             ])
             
-        self.x = "path"
+        self.x = "id"
         self.y = "discretized_diagnosis"
         
         if self.train:
